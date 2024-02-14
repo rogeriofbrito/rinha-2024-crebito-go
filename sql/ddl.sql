40130db74b9323957ef117029afa40eec0ce10a5
Rogério <rogeriobritoufu@gmail.com>
2024-02-14 13:06:33 -0300

    Creating sql scripts

---
+++ sql/ddl.sql
@@ -0,0 +1,18 @@
+CREATE TABLE public.client (
+	id int4 NOT NULL,
+	"limit" int4 NOT NULL,
+	balance int4 NOT NULL,
+	CONSTRAINT client_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE public."transaction" (
+	id serial4 NOT NULL,
+	client_id int4 NOT NULL,
+	"type" int4 NOT NULL,
+	value int4 NOT NULL,
+	description varchar NOT NULL, -- TODO: limit in 10 char
+	created_at timestamp with time zone NOT NULL,
+	CONSTRAINT transaction_pkey PRIMARY KEY (id)
+);
+
+ALTER TABLE public."transaction" ADD CONSTRAINT transaction_fk FOREIGN KEY (client_id) REFERENCES public.client(id);
